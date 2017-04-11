4d7e4fccdb8249ec81ceeb7e4008edfd7d11ed05
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-04-11 21:59:54 +0200

    build the complete toolchain instead of bootstrap compiler

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# hermit-bootstrap
+# hermit-toolchain
 
-This repository contains scripts to build the HermitCore's bootstrap compiler, which is the minimal version of the gcc to build the library OS [HermitCore](http://www.hermitcore.org).
-To build the compiler just call the script as follow:
+This repository contains scripts to build the HermitCore's cross-compiler, which based on the library OS [HermitCore](http://www.hermitcore.org).
+To build the toolchain just call the script as follow:
 
 ```bash
+$ . ./local-cmake.sh
 $ . ./bootstrap.sh x86_64-hermit /home/usr/hermit
 ```
 
 The first argument of the script specifies the target architecture, where the second argument defines the path to the installation directory.
+Do create the toolchain, write access to the installation directory is required.
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,9 @@
 # $1 = specifies the target architecture
 # $2 = specifies the installation directory
 
-BUILDDIR=build_toolchain
+BUILDDIR=build
 CLONE_DEPTH="--depth=50"
-PREFIX="$2"/opt/hermit
+PREFIX="$2"
 TARGET=$1
 NJOBS=-j"$(nproc)"
 PATH=$PATH:$PREFIX/bin
@@ -24,6 +24,27 @@ fi
 
 if [ ! -d "gcc" ]; then
 git clone $CLONE_DEPTH https://github.com/RWTH-OS/gcc.git
+wget ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.15.tar.bz2 -O isl-0.15.tar.bz2
+tar jxf isl-0.15.tar.bz2
+mv isl-0.15 gcc/isl
+fi
+
+if [ ! -d "hermit" ]; then
+git clone --recursive -b devel https://github.com/RWTH-OS/HermitCore.git hermit
+fi
+
+if [ ! -d "newlib" ]; then
+git clone $CLONE_DEPTH https://github.com/RWTH-OS/newlib.git
+fi
+
+if [ ! -d "pte" ]; then
+git clone $CLONE_DEPTH https://github.com/RWTH-OS/pthread-embedded.git pte
+cd pte
+./configure --prefix=$PREFIX
+fi
+
+if [ ! -d "libomp" ]; then
+git clone $CLONE_DEPTH https://github.com/RWTH-OS/libomp_oss.git libomp
 fi
 
 if [ ! -d "tmp/binutils" ]; then
@@ -37,6 +58,51 @@ if [ ! -d "tmp/bootstrap" ]; then
 mkdir -p tmp/bootstrap
 cd tmp/bootstrap
 ../../gcc/configure --target=$TARGET --prefix=$PREFIX --without-headers --disable-multilib --with-isl --enable-languages=c,c++,lto --disable-nls --disable-shared --disable-libssp --disable-libgomp --enable-threads=posix --enable-tls --enable-lto --disable-symvers && make $NJOBS all-gcc && make install-gcc
+cd -
+fi
+
+if [ ! -d "tmp/hermit" ]; then
+mkdir -p tmp/hermit
+cd tmp/hermit
+cmake -DTOOLCHAIN_BIN_DIR=$PREFIX/bin -DCMAKE_INSTALL_PREFIX=$PREFIX -DBOOTSTRAP=true ../../hermit
+make hermit-bootstrap
+make hermit-bootstrap-install
+cd -
+fi
+
+if [ ! -d "tmp/newlib" ]; then
+mkdir -p tmp/newlib
+cd tmp/newlib
+../../newlib/configure --target=$TARGET --prefix=$PREFIX --disable-shared --disable-multilib --enable-lto --enable-newlib-hw-fp --enable-newlib-io-c99-formats --enable-newlib-multithread && make $NJOBS && make install
+cd -
+fi
+
+cd pte
+make && make install
+cd ..
+
+if [ ! -d "tmp/gcc" ]; then
+mkdir -p tmp/gcc
+cd tmp/gcc
+../../gcc/configure --target=$TARGET --prefix=$PREFIX --with-newlib --with-isl --disable-multilib --without-libatomic --enable-languages=c,c++,go,fortran,lto --disable-nls --disable-shared --disable-libssp --enable-threads=posix --disable-libgomp --enable-tls --enable-lto --disable-symver && make $NJOBS && make install
+cd -
+fi
+
+if [ ! -d "tmp/libomp" ]; then
+mkdir -p tmp/libomp
+cd tmp/libomp
+cmake -DTOOLCHAIN_BIN_DIR=$PREFIX/bin -DCMAKE_INSTALL_PREFIX=$PREFIX/$TARGET -DHERMIT=1 ../../libomp
+make
+make install
+cd -
+fi
+
+if [ ! -d "tmp/final" ]; then
+mkdir -p tmp/final
+cd tmp/final
+cmake -DTOOLCHAIN_BIN_DIR=$PREFIX/bin -DCMAKE_INSTALL_PREFIX=$PREFIX ../../hermit
+make
+make install
 cd -
 fi
 

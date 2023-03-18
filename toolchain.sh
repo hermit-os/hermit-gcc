30babb7c20d3d9b1833aa944e20a62baa60ce5e4
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2023-03-18 13:04:44 +0100

    enable PIE support

--- toolchain.sh
+++ toolchain.sh
@@ -16,11 +16,11 @@ NJOBS=-j"$(nproc)"
 PATH=$PATH:$PREFIX/bin
 export CFLAGS="-w"
 export CXXFLAGS="-w"
-export CFLAGS_FOR_TARGET="-m64 -O3"
-export GOFLAGS_FOR_TARGET="-m64 -O3"
-export FCFLAGS_FOR_TARGET="-m64 -O3"
-export FFLAGS_FOR_TARGET="-m64 -O3"
-export CXXFLAGS_FOR_TARGET="-m64 -O3"
+export CFLAGS_FOR_TARGET="-m64 -O3 -fPIE"
+export GOFLAGS_FOR_TARGET="-m64 -O3 -fPIE"
+export FCFLAGS_FOR_TARGET="-m64 -O3 -fPIE"
+export FFLAGS_FOR_TARGET="-m64 -O3 -fPIE"
+export CXXFLAGS_FOR_TARGET="-m64 -O3 -fPIE"
 
 echo "Build bootstrap toolchain for $TARGET with $NJOBS jobs for $PREFIX"
 
